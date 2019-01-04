ca4659c0781cd6c436f14888e7a6a6d1bda76787
yeon <yeonyya@gmail.com>
2019-01-04 12:43:54 +0900

    뱀이 과일먹는 게임

---
+++ Assets/02.Scripts/FoodConsumer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+public class FoodConsumer : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "food")
+        {
+            collision.gameObject.SetActive(false);
+            Slithering s = GetComponentInParent<Slithering>();
+            if (s != null)
+            {
+                s.AddBodyPart();
+            }
+        }
+    }
+}
